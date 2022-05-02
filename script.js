f121562b619895c93dac14ee27f45eb3a3ea3db6
ttfmma <ttfmma@protonmail.com>
2022-05-02 00:41:34 -0700

    added pic slider for coaches

--- script.js
+++ script.js
@@ -46,3 +46,35 @@ for (let item of items) {
   item.addEventListener("keypress", toggleItem, false);
 }
 document.addEventListener("click", closeSubmenu, false);
+
+/* Coaches slider */
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function nextSlide(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+      slideIndex = 1
+    }    
+  if (n < 1) {
+      slideIndex = slides.length
+    }
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "flex";  
+  dots[slideIndex-1].className += " active";
+}
